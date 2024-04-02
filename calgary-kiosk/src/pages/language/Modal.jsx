e069ea525a6e07fc60500635a75065c49300ac25
GauravGulatii <78321046+GauravGulatii@users.noreply.github.com>
2024-04-01 19:59:20 -0600

    Update Modal.jsx

--- calgary-kiosk/src/pages/language/Modal.jsx
+++ calgary-kiosk/src/pages/language/Modal.jsx
@@ -1,14 +1,29 @@
 // Modal.js
-import React from 'react';
-import './Modal.css'; 
+import React, { useEffect, useState } from 'react';
+import './Modal.css';
 
-const Modal = ({ showModal, setShowModal, message, onConfirm }) => {
+const Modal = ({ showModal, setShowModal, message, onConfirm, countdown }) => {
+  const [timer, setTimer] = useState(countdown);
+
+  useEffect(() => {
+    if (showModal && timer > 0) {
+      const timeout = setTimeout(() => {
+        setTimer(timer - 1);
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    } else if (timer === 0) {
+      onConfirm();
+    }
+  }, [showModal, timer, onConfirm]);
+
   if (!showModal) return null;
 
   return (
     <div className="modal-backdrop">
       <div className="modal-content">
         <p>{message}</p>
+        <p>Redirecting to Home Screen in {timer} seconds...</p>
         <button onClick={() => {
           onConfirm();
           setShowModal(false);
