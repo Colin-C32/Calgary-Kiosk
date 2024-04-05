06d5c3b6be67dc8fea37aa9c5a8c44e56c31593d
Abdullah Ishtiaq <ishtiaqabdullah11@gmail.com>
2024-04-05 03:04:31 -0600

    Combined 2 files into 1 and made more efficient

--- calgary-kiosk/src/pages/transit/transitRoutes/TransitRoutes.jsx
+++ calgary-kiosk/src/pages/transit/transitRoutes/TransitRoutes.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import GoogleMap from '../../../components/google-maps/google-maps';
+/* global google */
+
 import { Header } from "../../../components/UI/Header" 
+import React, { useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 
 const TransitRoutes = ({ setPage }) => {
-  const apiKey = 'AIzaSyDywEmVrxAsXW4uDgQUSn3xZlQxkbC8syM'; // Replace with your actual API key
+  const apiKey = 'AIzaSyDywEmVrxAsXW4uDgQUSn3xZlQxkbC8syM'; 
+
+  const GoogleMap = ({ apiKey }) => {
+    const mapContainerRef = useRef(null);
+  
+    useEffect(() => {
+      const loader = new Loader({
+        apiKey,
+        version: 'weekly', // Specify the version of the API
+      });
+  
+      loader.load().then(() => {
+        new google.maps.Map(mapContainerRef.current, {
+          center: { lat: 40.7128, lng: -74.006 },
+          zoom: 10,
+        });
+      });
+    }, [apiKey]);
+  
+    return <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />;
+  };
 
   return (
     <div>
@@ -12,7 +34,9 @@ const TransitRoutes = ({ setPage }) => {
         previousPage="Transit"
         title="Transit Routes"
       />
+      <div className= "google-map" style={{height: 200}}>
       <GoogleMap apiKey={apiKey} />
+      </div>
     </div>
   );
 };
